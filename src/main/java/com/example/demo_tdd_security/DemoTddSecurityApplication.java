372963c78283a9b5d0cffc28301b4b7a39ce69c0
EON-DC <mun0392@gmail.com>
2022-12-02 07:36:09 +0900

    tdd spring-security, jwt, jpa

---
+++ src/main/java/com/example/demo_tdd_security/DemoTddSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_tdd_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTddSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTddSecurityApplication.class, args);
+    }
+
+}
